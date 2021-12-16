14317f926670c81d61d2e187148f7fbbfc296a0d
sashaz <6964653+OZahnitko@users.noreply.github.com>
2021-12-16 12:34:42 +1000

    more composition

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "tko-component-library";
+
 import { me } from "@components";
 
 export const App = () => {
-  return <div>App {me}</div>;
+  return (
+    <div>
+      <div>App {me}</div>
+      <Button />
+    </div>
+  );
 };
